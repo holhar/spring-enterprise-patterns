7f8364e7e0a61d499bee129a1b2005e80a94b0a7
Holger Harms <holhar@posteo.de>
2021-12-07 15:50:11 +0100

    initial commit

---
+++ src/main/java/de/holhar/spring/enterprisepatterns/SpringEnterprisePatternsApplication.java
@@ -0,0 +1,13 @@
+package de.holhar.spring.enterprisepatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEnterprisePatternsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEnterprisePatternsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/holhar/spring/enterprisepatterns/SpringEnterprisePatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package de.holhar.spring.enterprisepatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEnterprisePatternsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
